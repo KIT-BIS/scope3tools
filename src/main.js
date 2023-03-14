e6a6bf51167985c37af8232bc14c6565bfc5c0d0
vz064 <andreas.fritsch@kit.edu>
2023-03-14 19:05:02 +0000

    vue and grid integration

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// import router from './router'
+// 
+// import './assets/main.css'
+// 
+// const app = createApp(App)
+// 
+// app.use(router)
+// 
+// app.mount('#app')
+import { createApp } from "vue";
+import App from "./views/GridView.vue";
+
+createApp(App).mount("#app");
---
+++ src/views/GridView.vue
@@ -0,0 +1,42 @@
+<template>
+    <ag-grid-vue
+      style="width: 500px; height: 200px"
+      class="ag-theme-alpine"
+      :columnDefs="columnDefs"
+      :rowData="rowData"
+    >
+    </ag-grid-vue>
+</template>
+  
+<script>
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+import { AgGridVue } from "ag-grid-vue3";
+import csv from '../data/scope3tools.csv'
+
+export default {
+  name: "App",
+  components: {
+    AgGridVue,
+  },
+  setup() {
+    console.log(csv);
+    return {
+      columnDefs: [
+      //  { headerName: "Make", field: "make" },
+      //  { headerName: "Model", field: "model" },
+      //  { headerName: "Price", field: "price" },
+        { headerName: "Name", field: "Name"},
+        { headerName: "Kategorie", field: "Beschreibung"},
+        { headerName: "Komplexität", field: "Komplexität"}
+      ],
+      rowData: csv,
+      //rowData: [
+      //  { make: "Toyota", model: "Celica", price: 35000 },
+      //  { make: "Ford", model: "Mondeo", price: 32000 },
+      //  { make: "Porsche", model: "Boxster", price: 72000 },
+      //],
+    };
+  },
+};
+</script>
